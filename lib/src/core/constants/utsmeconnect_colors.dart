8adb8cabd28159bd0073dcb854e980d927671535
Michael Matta <micopero@live.com>
2024-07-04 13:58:14 +1000

    Pushing the essential files only, added a gitignore file

---
+++ lib/src/core/constants/utsmeconnect_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class UTSMEConnectColors {
+  static const Color kBackgroundPrimary = Color.fromARGB(255, 0, 92, 128);
+  static const Color kBackgroundSecondary = Color.fromARGB(255, 45, 45, 45);
+  static const Color kSelectedItem = Color.fromARGB(255, 246, 205, 21);
+  static const Color kTextFieldBorder = Color.fromARGB(255, 53, 53, 53);
+  static const Color kTextFieldErrorBorder = Color.fromARGB(255, 247, 53, 53);
+}
